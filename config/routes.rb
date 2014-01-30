5a4ed0a819f0f4b3e90f17e447c2fad450501f4e
kaia9 <josh.sylvester@gmail.com>
2014-01-29 19:15:29 -0700

    first update, getting github connection working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Advm::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+  match 'api/github-update'  => 'api#github_pull'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
