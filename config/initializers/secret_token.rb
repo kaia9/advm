38be694d7205243cec362ed752fddbfd16965aeb
kaia9 <josh.sylvester@gmail.com>
2014-01-29 18:24:48 -0700

    default rails environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Advm::Application.config.secret_token = 'a6b09df5cb7dfccade8ad3e7151590e03ee26123ad9233806e425400bacee0922bbdbc09ffae2fcb4668fc1e6912d3df85062f7dd2ed8fabda95c3426d157bfe'
